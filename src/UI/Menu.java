884412c0a645bc5539bf19a38001668ac7ada61b
aikaterinikoutsou <aikaterini.koutsou@ceu.es>
2024-04-10 20:21:22 +0200

    jdbc manager introduction

--- src/UI/Menu.java
+++ src/UI/Menu.java
@@ -1,10 +1,15 @@
 package UI;
 
+import VetClinicJDBC.JDBCManager;
+
 public class Menu {
 
+	private static JDBCManager jdbcmanager;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		jdbcmanager = new JDBCManager();
 	}
 
 }
--- src/VetClinicJDBC/JDBCManager.java
+++ src/VetClinicJDBC/JDBCManager.java
@@ -1,5 +1,43 @@
 package VetClinicJDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCManager {
 
+	private Connection c = null;
+	
+	public JDBCManager() {
+		
+		try {
+			
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:./db/vetclinic.db");
+			c.createStatement().execute("PRAGMA foreign_keys=ON");
+			
+			System.out.print("Database Connection opened.");
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException e) {
+			System.out.print("Libraries not loaded");
+		}
+	}
+	
+	public Connection getConnection() {
+		return c;
+	}
+	
+	public void disconnect()
+	{
+		try {
+			c.close();
+		}
+		catch(SQLException e){ 
+			e.printStackTrace();
+		}		
+	}
 }
