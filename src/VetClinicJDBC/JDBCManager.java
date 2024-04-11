763b9ef0f3a28ed25bb2d3e1bba326b6e5bcf1bb
aikaterinikoutsou <aikaterini.koutsou@ceu.es>
2024-04-11 19:14:46 +0200

    finish jdbcmanager

--- src/VetClinicJDBC/JDBCManager.java
+++ src/VetClinicJDBC/JDBCManager.java
@@ -3,6 +3,7 @@ package VetClinicJDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCManager {
 
@@ -17,6 +18,7 @@ public class JDBCManager {
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			
 			System.out.print("Database Connection opened.");
+			this.createTables();
 			
 		}
 		catch(SQLException e) {
@@ -25,6 +27,43 @@ public class JDBCManager {
 		catch(ClassNotFoundException e) {
 			System.out.print("Libraries not loaded");
 		}
+	}
+	
+	private void createTables() {
+		try {
+			
+			Statement stmt = c.createStatement();
+			
+			String sql = "CREATE TABLE owners ("
+					+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "name text NOT NULL, phone INTEGER, "
+					+ "cardnumber TEXT, email TEXT NOT NULL);";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE pets ("
+					+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "name TEXT NOT NULL, cured BOOLEAN, "
+					+ "typeOfAnimal TEXT CHECK (typeOfAnimal in (dog, cat, bird, hamster)),"
+					+ "coat TEXT, dob DATE NOT NULL, onwer_id INTEGER REFERENCES owners(id));";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE vets(id INTEGER PRIMARY KEY  AUTOINCREMENT,"
+					+"name TEXT NOT NULL, specialty TEXT, phone INTEGER,"
+					+ "email TEXT NOT NULL, licence TEXT);";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE treats (vet_id INTEGER REFERENCES vets(id),"
+					+ "pet_id REFERENCES pets(id), PRIMARY KEY(vet_id,pet_id) );";
+			stmt.executeUpdate(sql);
+			
+			
+		}catch(SQLException e) {
+			if(!e.getMessage().contains("already exists")) 
+			{
+				e.printStackTrace();
+			}			
+		}
+		
 	}
 	
 	public Connection getConnection() {
