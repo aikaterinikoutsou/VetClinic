d39f70fd61f83bf36d0551024809563b25df17b0
aikaterinikoutsou <aikaterini.koutsou@ceu.es>
2024-04-11 19:32:15 +0200

    jdbcOwnerMAnager and createowner

---
+++ src/VetClinicIfaces/OwnerManager.java
@@ -0,0 +1,8 @@
+package VetClinicIfaces;
+
+import VetClinicPOJOs.Owner;
+
+public interface OwnerManager {
+
+	public void createOwner(Owner o);
+}
--- src/VetClinicJDBC/JDBCOwnerManager.java
+++ src/VetClinicJDBC/JDBCOwnerManager.java
@@ -1,5 +1,41 @@
 package VetClinicJDBC;
 
-public class JDBCOwnerManager {
+import java.sql.PreparedStatement;
+
+import VetClinicIfaces.OwnerManager;
+import VetClinicPOJOs.Owner;
+
+public class JDBCOwnerManager implements OwnerManager{
+	
+	private JDBCManager manager;
+	
+	public JDBCOwnerManager (JDBCManager m) {
+		this.manager = m;
+	}
+
+	@Override
+	public void createOwner(Owner o) {
+		// TODO Auto-generated method stub
+		try {
+			String sql= "INSERT INTO owners (name,"
+					+ "phone, email, cardnumber)"
+					+ "VALUES (?,?,?,?)";
+			
+			PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+			prep.setString(1, o.getName());
+			prep.setInt(2, o.getPhone());
+			prep.setString(3, o.getEmail());
+			prep.setInt(4, o.getCardnumber());
+			
+			prep.executeUpdate();				
+					
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 }
